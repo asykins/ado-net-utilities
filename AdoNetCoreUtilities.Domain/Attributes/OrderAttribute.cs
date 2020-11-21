c433dd45302ac6ba5ab048d795246ca63e53a839
thomas.halin <thomas.halin.th@gmail.com>
2020-11-21 18:28:50 +0100

    Removed AllowMultiple on Order Attribute

--- AdoNetCoreUtilities.Domain/Attributes/OrderAttribute.cs
+++ AdoNetCoreUtilities.Domain/Attributes/OrderAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace AdoNetCoreUtilities.Domain.Attributes
 {
-    [System.AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    [System.AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class OrderAttribute : Attribute
     {
         public readonly int columnOrder ;
