e1e979dc510ac285f730b15a557b28550e1ab7b6
thomas.halin <thomas.halin.th@gmail.com>
2020-11-21 18:40:13 +0100

    Added ISqlRepository to the solution

---
+++ AdoNetCoreUtilities.Domain/Interfaces/ISqlRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetCoreUtilities.Domain.Interfaces
+{
+    public interface ISqlRepository<T>
+    {
+        Task<IEnumerable<T>> GetAsync();
+        Task<IEnumerable<T>> GetAsync(params Func<T, bool>[] functions);
+        Task InsertOrUpdateAsync(IEnumerable<T> source);
+        Task BulkInsert(IEnumerable<T> source, string tableName = null);
+    }
+}
